457c08a22780bee625c86b56fb591669b5c9350d
Brenden D <90849787+Brenden211@users.noreply.github.com>
2025-03-29 13:07:23 -0400

    0.0.3
    Created the main menu UI and made some changes to the project setting for the UI.

--- email.gd
+++ email.gd
@@ -5,17 +5,17 @@ func _on_exit_button_pressed() -> void:
 	print("Exit button pressed")
 
 # When the email 1 button is pressed from the list change the text
-func but_email_1_pressed() -> void:
+func email_1_button_pressed() -> void:
 	# Set labels text
 	$"Details Con/MarginContainer/Email/Heading".text = "EMAIL 1"
 	$"Details Con/MarginContainer/Email/Description".text = "This is a payment email."
 
-func but_email_2_pressed() -> void:
+func email_2_button_pressed() -> void:
 	# Set labels text
 	$"Details Con/MarginContainer/Email/Heading".text = "EMAIL 2"
 	$"Details Con/MarginContainer/Email/Description".text = "This is a SCAM email."
 
-func but_email_3_pressed() -> void:
+func email_3_button_pressed() -> void:
 	# Set labels text
 	$"Details Con/MarginContainer/Email/Heading".text = "EMAIL 3"
 	$"Details Con/MarginContainer/Email/Description".text = "*****************."
